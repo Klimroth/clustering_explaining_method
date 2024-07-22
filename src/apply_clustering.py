4ea8de67f64cc311a4cc15bde7611c33e3a519b9
Klimroth <klimroth@gmx.de>
2024-07-22 17:09:02 +0200

    Added Gap statistics

--- src/apply_clustering.py
+++ src/apply_clustering.py
@@ -10,8 +10,6 @@ from gapstatistics.gapstatistics import GapStatistics
 from sklearn.neighbors import NearestCentroid
 from sklearn.cluster import AgglomerativeClustering
 import matplotlib.pyplot as plt
-import matplotlib.cm as mplcm
-import matplotlib.colors as colors
 import numpy as np
 
 import config
@@ -44,49 +42,7 @@ class ClusteringApplier:
         num_colors: int = config.GAP_STATISTIC_CLUSTER_RANGE
         cm = plt.get_cmap('gist_rainbow')
         color_dict = {i: cm(1.*i/num_colors) for i in range(num_colors)}
-        optimum, params = gs.fit_predict(K=config.GAP_STATISTIC_CLUSTER_RANGE, X=df.to_numpy())
+        gs.fit_predict(K=config.GAP_STATISTIC_CLUSTER_RANGE, X=df.to_numpy())
         gs.plot(original_labels=df.columns, colors=color_dict)
         plt.savefig(f"{config.OUTPUT_FOLDER_BASE}gapstat/{config.DATASET_NAME}_gap-statistic-plot.pdf", bbox_inches='tight')
-
-    @staticmethod
-    def do_gap_statistic(max_clust=20):
-        def clustering_method(X, k):
-            # Adjust here, if not ward's clustering algorithm should be taken
-            ward = AgglomerativeClustering(n_clusters=k, linkage="ward").fit_predict(X)
-            clf = NearestCentroid()
-            clf.fit(X, ward)
-            return clf.centroids_, ward
-
-        df: pd.DataFrame = pd.read_excel('/mnt/f/Nextcloud/Dierkes/Naturverbundenheit/Fragebogenclustering_V2_imputed.xlsx')
-        df_use = df[
-            ['Pollution', 'Climate change', 'Invasive species', 'Habitat loss', 'Overexploitation', 'Discrimination']]
-        df_use = df_use.rename(columns=COLUMNRENAME)
-
-        rcParams['figure.figsize'] = 18.7 / 2, 8.27 / 2
-
-        optimalK = OptimalK(clusterer=clustering_method, n_iter=20)
-
-        noise = np.random.normal(loc=0.0, scale=0.2, size=[len(df_use.index), 6])
-        df_use += noise
-
-        n_clusters = optimalK(df_use, cluster_array=range(2, max_clust))
-
-        plt.cla()
-        plt.clf()
-
-        plt.errorbar(optimalK.gap_df.n_clusters, optimalK.gap_df.gap_value, xerr=optimalK.gap_df.sk, marker='o',
-                     color='black')
-
-        plt.title('Gap statistic')
-        plt.xlabel('Number of Clusters')
-        plt.xticks(ticks=list(range(2, max_clust)))
-        plt.ylabel('Gap')
-
-        ax = plt.gca()
-        ax.grid(which='major', axis='x', linestyle='--')
-
-        optimalK.gap_df.to_excel(
-            '/mnt/f/Nextcloud/Dierkes/Naturverbundenheit/revision/drei_disruptoren/gap_statistic1.xlsx', index=False)
-        plt.savefig('/mnt/f/Nextcloud/Dierkes/Naturverbundenheit/revision/drei_disruptoren/gap_statistic1.pdf', dpi=300,
-                    bbox_inches='tight')
 
